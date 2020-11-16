f22e872db90ac22c3f5d9b953a83ccbbb1480438
Othman Taik <71637757+Otty97@users.noreply.github.com>
2020-11-16 16:13:08 +0100

    Completed

---
+++ js/vue.js
@@ -0,0 +1,8 @@
+var root = new Vue({
+  el: '#root',
+  data: {
+    message: 'VUE Js',
+    show: false,
+    logo: 'https://www.laramind.com/blog/wp-content/uploads/2019/09/Untitled-design-2019-09-03T162841.927-620x420.png',
+  }
+})
